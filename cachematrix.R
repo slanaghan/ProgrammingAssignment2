6824794c303a22ede20ac426c49870f5df823885
slanaghan <slanaghan@gmail.com>
2015-10-22 18:28:18 -0700

    Cleaned up and added comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,38 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do  
+## makeCacheMatrix and cacheSolve together return the inverse of a matrix
+## either by calculating it, or retrieving it if it is stored.
 
-## Write a short comment describing this function
+## makeCacheMatrix takes as input an invertible matrix and returns an object 
+## that stores the matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
   minverse <- NULL
   set <- function(y) {
     x <<- y
-    minverse <<- NULL
+    minverse <<- NULL                      
   }
-  get <- function() x
-  setinverse <- function(solve) minverse <<- solve
-  getinverse <- function() minverse
+  get <- function() x # Return original matrix
+  setinverse <- function(solve) minverse <<- solve # Store inverse of original matrix
+  getinverse <- function() minverse # Return inverse of original matrix
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes the ouput of makeCacheMatrix (containing a function and 
+## its inverse, if already calculated), and returns the inverse of the function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  #m <- makeCacheMatrix(x)
-  #x$setinverse()
-  #print(x)
-  minverse <- x$getinverse()
-  #minverse
-  if(!is.null(minverse)) {
+  minverse <- x$getinverse()               # Get matrix inverse from input vector
+  if(!is.null(minverse)) {                 # If inverse is already cached, get it from vector
     message("getting cached data")
     return(minverse)
   }
-  data <- x$getinverse()
-  minverse <- solve(x$get())
-  x$setinverse(minverse)
-  minverse
+  minverse <- solve(x$get())               # If inverse is not cached, find the inverse,
+  x$setinverse(minverse)                   # then store inverse in the input vector in parent
+  minverse                                 # environment.
 }
